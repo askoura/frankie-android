e9b9f39a59ba0eaf2548531c5296338e5dbdf83d
Amr Askoura <>
2023-02-12 11:12:59 +0100

    expression mnager integration

---
+++ app/src/main/java/com/example/reactfromassets/EMNavProcessor.kt
@@ -0,0 +1,68 @@
+package com.example.reactfromassets
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.Base64
+import android.webkit.*
+import com.frankie.expressionmanager.ext.ScriptUtils
+import com.frankie.expressionmanager.model.NavigationUseCaseInput
+import com.frankie.expressionmanager.usecase.NavigationJsonOutput
+import com.frankie.expressionmanager.usecase.NavigationUseCaseWrapperImpl
+import com.frankie.expressionmanager.usecase.ScriptEngine
+import com.frankie.expressionmanager.usecase.ValidationJsonOutput
+import kotlinx.coroutines.*
+
+@SuppressLint("SetJavaScriptEnabled")
+class EMNavProcessor constructor(
+    context: Context
+) {
+    val webView = WebView(context)
+
+    init {
+        webView.clearCache(true)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+        webView.settings.domStorageEnabled = true
+        loadJavaScript(ScriptUtils().initialScript + "\n" + ScriptUtils().commonScript)
+    }
+
+    private fun loadJavaScript(input: String) {
+        val javascript = "<script>$input</script>"
+        val data = javascript.toByteArray(charset("UTF-8"))
+        val base64 = Base64.encodeToString(data, Base64.DEFAULT)
+        webView.loadUrl("data:text/html;charset=utf-8;base64,$base64")
+
+    }
+
+    fun koko(
+        validationJsonOutput: ValidationJsonOutput,
+        navigationUseCaseInput: NavigationUseCaseInput,
+        navListener: NavigationListener
+    ) {
+        val navigationUseCaseWrapperImpl = NavigationUseCaseWrapperImpl(
+            object : ScriptEngine {
+                override fun executeScript(method: String, script: String): String {
+                    throw java.lang.IllegalStateException("Should not resort to Script engine")
+                }
+            },
+            validationJsonOutput,
+            navigationUseCaseInput
+        )
+        val script = navigationUseCaseWrapperImpl.getNavigationScript()
+        webView.evaluateJavascript("JSON.parse(navigate($script))") { value ->
+            value?.let {
+                try {
+                    navListener.onResult(navigationUseCaseWrapperImpl.processNavigationResult(value))
+                } catch (e: Exception) {
+                    navListener.onError(e)
+                }
+            }
+        }
+    }
+}
+
+interface NavigationListener {
+    fun onResult(navigationJsonOutput: NavigationJsonOutput)
+    fun onError(error: Throwable)
+
+}
--- app/src/main/java/com/example/reactfromassets/MainActivity.kt
+++ app/src/main/java/com/example/reactfromassets/MainActivity.kt
@@ -6,33 +6,51 @@ import android.util.Log
 import android.webkit.*
 import androidx.appcompat.app.AppCompatActivity
 import com.example.reactfromassets.databinding.ActivityMainBinding
-import com.frankie.expressionmanager.usecase.ScriptEngine
-import com.frankie.expressionmanager.usecase.ValidationUseCaseWrapperImpl
+import com.fasterxml.jackson.module.kotlin.jacksonTypeRef
+import com.frankie.expressionmanager.model.NavigationUseCaseInput
+import com.frankie.expressionmanager.model.SurveyLang
+import com.frankie.expressionmanager.model.jacksonKtMapper
+import com.frankie.expressionmanager.usecase.NavigationJsonOutput
+import com.frankie.expressionmanager.usecase.ValidationJsonOutput
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
+    private lateinit var emNavProcessor: EMNavProcessor
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
+        emNavProcessor = EMNavProcessor(this)
         setContentView(binding.root)
         binding.webview.loadUrl("file:///android_asset/build/index.html?url=18.198.94.221:8081&sid=1")
-        val impl = ValidationUseCaseWrapperImpl(
-            object : ScriptEngine {
-                override fun executeScript(method: String, script: String): String {
-                    return ""
-                }
-            },
-            application.assets.open("surveyFile.json").bufferedReader().use {
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.root.postDelayed({ navigate() }, 2000)
+    }
+
+    fun navigate() {
+        val processedComponents: String =
+            application.assets.open("processed_1.json").bufferedReader().use {
                 it.readText()
             }
-        )
+        val validationJsonOutput: ValidationJsonOutput =
+            jacksonKtMapper.readValue(processedComponents, jacksonTypeRef<ValidationJsonOutput>())
+        val navigationUseCaseInput = NavigationUseCaseInput(defaultLang = SurveyLang.EN)
+        emNavProcessor.koko(
+            validationJsonOutput,
+            navigationUseCaseInput,
+            object : NavigationListener {
+                override fun onResult(navigationJsonOutput: NavigationJsonOutput) {
+                    Log.v("bla", navigationJsonOutput.toString())
+                }
 
-        val koko = impl.validate()
-        Log.v("blah", koko.toString())
-
+                override fun onError(error: Throwable) {
+                    error.printStackTrace()
+                }
+            })
     }
 }
