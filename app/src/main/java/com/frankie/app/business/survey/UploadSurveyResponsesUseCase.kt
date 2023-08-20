859e4463e3c04f90bee2aa29e27e4f4303fac7b4
Amr Askoura <>
2023-08-20 14:54:19 +0200

    use version from response

--- app/src/main/java/com/frankie/app/business/survey/UploadSurveyResponsesUseCase.kt
+++ app/src/main/java/com/frankie/app/business/survey/UploadSurveyResponsesUseCase.kt
@@ -5,6 +5,7 @@ import com.frankie.app.AppEvent
 import com.frankie.app.EventBus
 import com.frankie.app.api.survey.UploadResponseRequestData
 import com.frankie.app.business.responses.ResponseRepository
+import com.frankie.app.db.model.Response
 import com.frankie.app.db.model.Response.Companion.ACTUAL_FILENAME_KEY
 import com.frankie.app.db.model.Response.Companion.STORED_FILENAME_KEY
 import com.frankie.app.ui.common.FileUtils
@@ -38,7 +39,11 @@ class UploadSurveyResponsesUseCaseImpl(
             .filter {
                 it.surveyStatus == SurveyStatus.ACTIVE && it.localUnsyncedResponsesCount > 0
             }.forEach {
-                uploadSurvey(it.id)
+                try {
+                    uploadSurvey(it.id)
+                } catch (e: java.lang.Exception) {
+                    reportError(e)
+                }
             }
         emit(Result.success(Unit))
     }.catch {
@@ -47,81 +52,91 @@ class UploadSurveyResponsesUseCaseImpl(
 
     private suspend fun uploadSurvey(surveyId: String) {
         eventBus.emitEvent(AppEvent.UploadingSurveyResponse(surveyId))
-        // 1. upload files
         val responses = responseRepository.getResponses(surveyId)
             .single().getOrThrow()
             .filter { !it.isSynced }
         val surveyDbEntity = surveyRepository.getSurveyDbEntity(surveyId).single().getOrThrow()
             ?: throw Exception("Survey not found")
-        val publishInfo = surveyDbEntity.publishInfoEntity
         val fileQuestions = surveyDbEntity.fileQuestions.map { "${it}.value" }
         responses.forEach { response ->
-            val voiceRecordingFilenames =
-                response.events.filterIsInstance<ResponseEvent.VoiceRecording>()
-                    .mapIndexed { index, it ->
-                        FileUploadInfo(
-                            it.fileName,
-                            "voice_recording_$index.3gp"
-                        )
-                    }
-            val filenames = response.values.filterKeys(fileQuestions::contains)
-                .values.mapNotNull { value ->
-                    (value as? Map<*, String>)?.let {
-                        FileUploadInfo(it[STORED_FILENAME_KEY]!!, it[ACTUAL_FILENAME_KEY]!!)
-                    }
-                }
-
-            val allFilenames = filenames + voiceRecordingFilenames
-            allFilenames.forEach { filename ->
-                val file = FileUtils.getResponseFile(appContext, filename.storedFileName, surveyId)
-                if (file.exists()) {
-                    val result = surveyRepository.uploadSurveyResponseFile(
-                        surveyId,
-                        filename.originalFileName,
-                        file
-                    ).single()
-                    if (result.isSuccess) {
-                        // 2. delete file after upload
-                        try {
-                            file.delete()
-                        } catch (e: Exception) {
-                            reportError(e)
-                            e.printStackTrace()
-                        }
-                    } else {
-                        reportError(result.exceptionOrNull())
-                    }
-                } else {
-                    reportError(IllegalStateException("File not found: $filename"))
-                }
-            }
-
-            // 3. upload response row
-            val uploadData = UploadResponseRequestData(
-                versionId = publishInfo.version,
-                lang = response.lang,
-                events = response.events,
-                values = response.values,
-                startDate = response.startDate,
-                submitDate = response.submitDate,
-                userId = response.userId,
-                navigationIndex = response.navigationIndex
-            )
-            val result =
-                surveyRepository.uploadSurveyResponse(surveyId, response.id, uploadData).single()
-            if (result.isSuccess) {
-                // 4. mark response as synced
-                responseRepository.markResponseAsSynced(response.id).collect()
-                surveyRepository.updateSurveyInDB(result.getOrThrow())
-                eventBus.emitEvent(AppEvent.UploadedSurveyResponse(surveyId))
-            } else {
-                reportError(result.exceptionOrNull())
+            try {
+                syncResponse(surveyId, response, fileQuestions)
+            } catch (e: Exception) {
+                reportError(e)
             }
         }
     }
 
+    @Suppress("UNCHECKED_CAST")
+    private suspend fun syncResponse(
+        surveyId: String,
+        response: Response,
+        fileQuestions: List<String>
+    ) {
+        val voiceRecordingFilenames =
+            response.events.filterIsInstance<ResponseEvent.VoiceRecording>()
+                .mapIndexed { index, it ->
+                    FileUploadInfo(
+                        it.fileName,
+                        "voice_recording_$index.3gp"
+                    )
+                }
+        val filenames = response.values.filterKeys(fileQuestions::contains)
+            .values.mapNotNull { value ->
+                (value as? Map<*, String>)?.let {
+                    FileUploadInfo(it[STORED_FILENAME_KEY]!!, it[ACTUAL_FILENAME_KEY]!!)
+                }
+            }
+
+        val allFilenames = filenames + voiceRecordingFilenames
+        allFilenames.forEach { filename ->
+            val file = FileUtils.getResponseFile(appContext, filename.storedFileName, surveyId)
+            if (file.exists()) {
+                val result = surveyRepository.uploadSurveyResponseFile(
+                    surveyId,
+                    filename.originalFileName,
+                    file
+                ).single()
+                if (result.isSuccess) {
+                    // 2. delete file after upload
+                    try {
+                        file.delete()
+                    } catch (e: Exception) {
+                        reportError(e)
+                    }
+                } else {
+                    reportError(result.exceptionOrNull())
+                }
+            } else {
+                reportError(IllegalStateException("File not found: $filename"))
+            }
+        }
+
+        // 3. upload response row
+        val uploadData = UploadResponseRequestData(
+            versionId = response.version,
+            lang = response.lang,
+            events = response.events,
+            values = response.values,
+            startDate = response.startDate,
+            submitDate = response.submitDate,
+            userId = response.userId,
+            navigationIndex = response.navigationIndex
+        )
+        val result =
+            surveyRepository.uploadSurveyResponse(surveyId, response.id, uploadData).single()
+        if (result.isSuccess) {
+            // 4. mark response as synced
+            responseRepository.markResponseAsSynced(response.id).collect()
+            surveyRepository.updateSurveyInDB(result.getOrThrow())
+            eventBus.emitEvent(AppEvent.UploadedSurveyResponse(surveyId))
+        } else {
+            reportError(result.exceptionOrNull())
+        }
+    }
+
     private fun reportError(throwable: Throwable?) {
-        // TODO: report error
+        throwable?.printStackTrace()
     }
 
     data class FileUploadInfo(val storedFileName: String, val originalFileName: String)
